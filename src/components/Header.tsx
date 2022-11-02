04f13978d40a281559b7f8ebf489a8dd998fdf35
Erick <erick.kuwahara79@gmail.com>
2022-11-02 13:39:30 -0300

    style: notifications

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -62,12 +62,16 @@ export function Header({ title }: HeaderProps) {
                 </button>
               </Popover.Trigger>
 
-              <Popover.Content className="w-full max-w-xs p-3 mx-auto transition-all ring-2 ring-gray-400 rounded bg-gray-800 animate-fade">
+              <Popover.Content className="w-full max-w-xs p-3 mx-auto transition-all ring-2 ring-gray-400 rounded bg-gray-800 animate-fade z-10">
                 <div className="flex gap-3">
                   <h3 className="text-sm">Notifications</h3>
                   <Popover.Close className="transition-opacity hover:opacity-60">
                     <X size={20} weight="bold" />
                   </Popover.Close>
+                </div>
+                <div className="mt-4">
+                  <p className="text-xs text-gray-200">System:</p>
+                  <p className="text-xs text-gray-100">- Nice to see you!</p>
                 </div>
                 <Popover.Arrow className="fill-gray-400" />
               </Popover.Content>
